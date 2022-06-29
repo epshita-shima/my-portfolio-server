b5697d5b590335e369cc7318f28b51b7c32dffd8
epshita-shima <epshitashima@gmail.com>
2022-06-29 21:58:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//use middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://portfolio_bd:7dImI8gtK32y3ne7@cluster0.y97ue.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const projectCollection = client.db("portfolio_project").collection("projects");
+
+        app.get('/projects', async (req, res) => {
+            const query = {};
+            const cursor = projectCollection.find(query);
+            const projects = await cursor.toArray();
+            res.send(projects);
+        })
+
+        app.get('/projects/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const project = await projectCollection.findOne(query);
+            res.send(project);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running my portfolio');
+});
+
+app.listen(port, () => {
+    console.log('portfolio server running');
+})
